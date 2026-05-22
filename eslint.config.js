1ebf0d10efc6f7e2a1ea183d37cd4fe2d83a0e46
Dushmilan <dushmilan05@gmail.com>
2026-05-22 19:58:46 +0530

    lint modification

--- eslint.config.js
+++ eslint.config.js
@@ -25,7 +25,7 @@ export default tseslint.config(
       ],
       "@typescript-eslint/no-unused-vars": [
         "error",
-        { argsIgnorePattern: "^_" },
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
       ],
     },
   },
